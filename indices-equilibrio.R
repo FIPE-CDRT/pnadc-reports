28709fc57c6b9ca2102d765cc0fd3225bd848540
Lucas Dias <lucasccdias0@gmail.com>
2021-01-10 19:33:48 -0300

    versao 1

--- indices-equilibrio.R
+++ indices-equilibrio.R
@@ -18,24 +18,26 @@ dir.create("tmp")
 pnadc_sp <- get_pnadc(year = 2019,
                       quarter = 4,
                       savedir = "tmp",
-                      vars = c("V2007", "V2010", "V4010", "V4013","VD4017", "VD4002"),
+                      vars = c("V2007", "V2010", "V4010", "VD4010","VD4017", "VD4002"),
                       design = FALSE) %>%
             dplyr::filter(UF == "São Paulo" & VD4002 == "Pessoas ocupadas") %>%
-            dplyr::mutate(cod = floor(as.numeric(V4010)/100),
-                                      cnae = floor(as.numeric(V4013)/1000),
-                                      mulher = ifelse(V2007 == "Mulher", 1, 0),
-                                      negro = ifelse(V2010 == "Preta" | V2010 == "Parda", 1, 0),
-                                      salario = VD4017) %>%
+            dplyr::mutate(cod = floor(as.numeric(V4010)/1000),
+                          setor = VD4010,
+                          mulher = ifelse(V2007 == "Mulher", 1, 0),
+                          negro = ifelse(V2010 == "Preta" | V2010 == "Parda", 1, 0),
+                          salario = VD4017,
+                          count = 1) %>%
             dplyr::filter(!is.na(salario))
 
 
 # proporções a nível setor-ocupação
 
 b_ocup_setor <- pnadc_sp %>% 
-                dplyr::group_by(cnae, cod) %>%
+                dplyr::group_by(setor, cod) %>%
                 dplyr::summarise(b_mulher = mean(mulher),
                                  b_negro = mean(negro),
-                                 massa_salarial = sum(salario))
+                                 massa_salarial = sum(salario),
+                                 n = sum(count))
 
 
 # participações a nível ocupação
@@ -47,22 +49,11 @@ p_ocup <- pnadc_sp %>%
 
 
 indices_setor <- b_ocup_setor %>% 
-                  dplyr::left_join(p_ocup) %>%
-                  dplyr::mutate(ie_racial = ((b_negro - p_negro)/p_negro)*((p_negro)/(1-p_negro))^(b_negro),
-                                ie_genero = ((b_mulher - p_mulher)/p_mulher)*((p_mulher)/(1-p_mulher))^(b_mulher)) %>%
-                  dplyr::mutate(ie_racial = ifelse(b_negro == 0, NA, ie_racial),
-                                ie_genero = ifelse(b_mulher == 0, NA, ie_genero))
-
-racial_setor <- indices_setor %>%
-                dplyr::ungroup() %>%
-                dplyr::filter(!is.na(ie_racial)) %>%
-                dplyr::group_by(cnae) %>%
-                dplyr::summarise(ie_racial = weighted.mean(ie_racial, massa_salarial))
-
-final_setor <- indices_setor %>%
-               dplyr::ungroup() %>%
-               dplyr::filter(!is.na(ie_genero)) %>%
-               dplyr::group_by(cnae) %>%
-               dplyr::summarise(ie_genero = weighted.mean(ie_genero, massa_salarial))
-
-
+                 dplyr::left_join(p_ocup) %>%
+                 dplyr::mutate(ie_racial = ((b_negro - p_negro)/p_negro)*((p_negro)/(1-p_negro))^(b_negro),
+                               ie_genero = ((b_mulher - p_mulher)/p_mulher)*((p_mulher)/(1-p_mulher))^(b_mulher)) %>%
+                 dplyr::ungroup() %>%
+                 dplyr::group_by(setor) %>%
+                 dplyr::summarise(ie_racial = weighted.mean(ie_racial, massa_salarial),
+                                  ie_genero = weighted.mean(ie_genero, massa_salarial))
+                  
