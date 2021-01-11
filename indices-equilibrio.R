1c9364de04d92af43175848b5383ee36517144bd
Lucas Dias <lucasccdias0@gmail.com>
2021-01-10 22:33:29 -0300

    plots

--- indices-equilibrio.R
+++ indices-equilibrio.R
@@ -3,14 +3,12 @@ library(PNADcIBGE)
 
 # define o local do script como diretório!
 
-### Falta: definir variável setorial (CNAE 2 dígitos é muito desagregado pra PNAD Trimestral)
-
-
 rstudioapi::getSourceEditorContext()$path %>%
   dirname() %>%
   setwd()
 
 dir.create("tmp")
+
 
 
 # pipe grande que trata quase tudo de uma vez
@@ -30,6 +28,7 @@ pnadc_sp <- get_pnadc(year = 2019,
             dplyr::filter(!is.na(salario))
 
 
+
 # proporções a nível setor-ocupação
 
 b_ocup_setor <- pnadc_sp %>% 
@@ -38,6 +37,7 @@ b_ocup_setor <- pnadc_sp %>%
                                  b_negro = mean(negro),
                                  massa_salarial = sum(salario),
                                  n = sum(count))
+
 
 
 # participações a nível ocupação
@@ -50,10 +50,43 @@ p_ocup <- pnadc_sp %>%
 
 indices_setor <- b_ocup_setor %>% 
                  dplyr::left_join(p_ocup) %>%
+                 dplyr::ungroup() %>%
                  dplyr::mutate(ie_racial = ((b_negro - p_negro)/p_negro)*((p_negro)/(1-p_negro))^(b_negro),
                                ie_genero = ((b_mulher - p_mulher)/p_mulher)*((p_mulher)/(1-p_mulher))^(b_mulher)) %>%
-                 dplyr::ungroup() %>%
                  dplyr::group_by(setor) %>%
                  dplyr::summarise(ie_racial = weighted.mean(ie_racial, massa_salarial),
-                                  ie_genero = weighted.mean(ie_genero, massa_salarial))
+                                  ie_genero = weighted.mean(ie_genero, massa_salarial)) %>%
+                 dplyr::filter(setor != "Atividades mal definidas")
                   
+
+
+#================================ Plotando =====================================
+
+
+dir.create("output")
+
+
+ggplot(indices_setor) + 
+  geom_bar(aes(x = ie_racial, y = reorder(setor, -ie_racial)), stat = 'identity') +
+  xlab("Índice de Equilíbrio Racial") +
+  theme_bw() +
+  theme(axis.title.y = element_blank(),
+        axis.text.y = element_text(size = 4))
+
+ggsave(file = "output/ie_racial.png", dpi = 600, width = 16, height = 9, units = "cm")
+
+
+ggplot(indices_setor) + 
+  geom_bar(aes(x = ie_genero, reorder(setor, -ie_genero)), stat = 'identity') +
+  xlab("Índice de Equilíbrio de Gênero") +
+  theme_bw() +
+  theme(axis.title.y = element_blank(),
+        axis.text.y = element_text(size = 4))
+
+ggsave(file = "output/ie_genero.png", dpi = 600, width = 16, height = 9, units = "cm")
+
+
+
+
+
+
